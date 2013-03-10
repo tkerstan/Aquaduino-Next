61058db3e50a97af95644d112be52fc123184288
Timo Kerstan <timo.kerstan@googlemail.com>
2013-03-10 20:41:00 +0100

    Removed auto-generated Eclipse makefiles

--- Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lArduino -lc -lTemplateParser -lTime -lEthernet -lSPI -lSD -lOneWire -lWebduino -lm
-
