af486b91d71952681799b3cf7ce64144c9c9596d
Timo Kerstan <timo.kerstan@googlemail.com>
2014-03-17 20:33:57 +0100

    Fixed merge issue

--- Sensors/DigitalInput.cpp
+++ Sensors/DigitalInput.cpp
@@ -71,3 +71,67 @@ uint8_t DigitalInput::getPin()
     return m_Pin;
 }
 
+int8_t DigitalInput::showWebinterface(WebServer* server,
+                                      WebServer::ConnectionType type, char* url)
+{
+    File templateFile;
+    TemplateParser* parser;
+    int16_t matchIdx;
+    char templateFileName[template_digitalinput_fnsize];
+    strcpy_P(templateFileName, template_digitalinput_fname);
+
+    if (type == WebServer::POST)
+    {
+        int8_t repeat;
+        char name[16], value[16];
+        do
+        {
+            repeat = server->readPOSTparam(name, 16, value, 16);
+            if (strcmp_P(name,
+                         (PGM_P) pgm_read_word(&(template_digitalinput_inputs[DI_I_PIN])))
+                == 0)
+            {
+                m_Pin = atoi(value);
+                pinMode(m_Pin, INPUT);
+            }
+        } while (repeat);
+        server->httpSeeOther(this->m_URL);
+    }
+    else
+    {
+        server->httpSuccess();
+        parser = __aquaduino->getTemplateParser();
+        templateFile = SD.open(templateFileName, FILE_READ);
+        while ((matchIdx =
+                parser->processTemplateUntilNextMatch(&templateFile,
+                                                      template_digitalinput,
+                                                      template_digitalinput_elements,
+                                                      server))
+               >= 0)
+        {
+            switch (matchIdx)
+            {
+            case DI_SNAME:
+                server->print(getName());
+                break;
+            case DI_PIN_NAME:
+                server->print((__FlashStringHelper *) pgm_read_word(template_digitalinput_inputs));
+                break;
+            case DI_PIN_VAL:
+                server->print(m_Pin);
+                break;
+            case DI_PIN_SIZE:
+                server->print(3);
+                break;
+            case DI_PIN_MAXLENGTH:
+                server->print(2);
+                break;
+
+            }
+        }
+
+        templateFile.close();
+    }
+    return true;
+}
+
