ef7078e9eeca7d83ab7ab8656b60c699b441a81d
Timo Kerstan <timo.kerstan@googlemail.com>
2014-04-15 21:42:20 +0200

    Removed main.cpp. Moved content to Aquaduino.cpp

--- Aquaduino.cpp
+++ Aquaduino.cpp
@@ -893,9 +893,6 @@ uint16_t Aquaduino::deserialize(void* data, uint16_t size)
     uint16_t nrOfControllers = 0;
     uint16_t nrOfSensors = 0;
     uint16_t calculatedSize = 0;
-    uint8_t* namePtr = 0;
-    uint8_t* typePtr = 0;
-    uint8_t* portPtr = 0;
     uint16_t i = 0;
 
     if (size >= 7)
@@ -2587,4 +2584,76 @@ int freeRam()
     extern int __heap_start, *__brkval;
     int v;
     return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
+}
+
+Aquaduino *aquaduino;
+
+void setup()
+{
+    int8_t i;
+/*    int8_t actuatorConfig[MAX_ACTUATORS] = ACTUATOR_CONFIG;
+    int8_t controllerConfig[MAX_CONTROLLERS] = CONTROLLER_CONFIG;
+    int8_t sensorConfig[MAX_SENSORS] = SENSOR_CONFIG;*/
+
+    aquaduino = new Aquaduino();
+
+/*    Serial.println(F("Initializing actuators..."));
+    for (i = 0; i < ACTUATORS; i++)
+    {
+        switch (actuatorConfig[i])
+        {
+            case ACTUATOR_DIGITALOUTPUT:
+                aquaduino->addActuator(new DigitalOutput(NULL, HIGH, LOW));
+                break;
+            default:
+            	Serial.println("Unknown actuator type");
+        }
+    }
+
+    Serial.println(F("Initializing controllers..."));
+    for (int i = 0; i < CONTROLLERS; i++)
+    {
+        switch (controllerConfig[i])
+        {
+            case CONTROLLER_LEVEL:
+                aquaduino->addController(new LevelController("Level"));
+                break;
+            case CONTROLLER_TEMPERATURE:
+                aquaduino->addController(new TemperatureController("Temperature"));
+                break;
+            case CONTROLLER_CLOCKTIMER:
+                aquaduino->addController(new ClockTimerController("Clock Timer"));
+                break;
+            default:
+            	Serial.println("Unknown controller type");
+        }
+    }
+
+    Serial.println(F("Initializing sensors..."));
+    for (int i = 0; i < SENSORS; i++)
+    {
+        switch (sensorConfig[i])
+        {
+            case SENSOR_DIGITALINPUT:
+                aquaduino->addSensor(new DigitalInput());
+                break;
+            case SENSOR_DS18S20:
+                aquaduino->addSensor(new DS18S20());
+                break;
+            case SENSOR_SERIALINPUT:
+                aquaduino->addSensor(new SerialInput());
+                break;
+            default:
+            	Serial.println("Unknown sensor type");
+        }
+    } */
+
+    aquaduino->initXively();
+
+    aquaduino->startTimer();
+}
+
+void loop()
+{
+    aquaduino->run();
 }
